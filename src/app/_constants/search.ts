1d178e2e4124ee2041b5a6b76264ba5d6f0a167c
Lucas <lucaslbmp@github.com>
2024-08-11 15:00:10 -0300

    feat: add quick search and BookingItem component

---
+++ src/app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOptions {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOptions[] = [
+  {
+    imageUrl: "/hair.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/beard.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/finishing.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/eyebrow.svg",
+    title: "Sobrancelha",
+  },
+  {
+    imageUrl: "/massage.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/hydration.svg",
+    title: "Hidratação",
+  },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,18 @@ import Header from "./_components/ui/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar } from "./_components/ui/avatar"
-import { AvatarImage } from "@radix-ui/react-avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/ui/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   const barberShops = await db.barbershop.findMany({})
+  const popularBarberShops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
   return (
     <main className="mb-5">
@@ -29,6 +33,21 @@ export default async function Home() {
           </Button>
         </div>
 
+        {/* BUSCA RAPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option, i) => (
+            <Button key={i} className="gap-2" variant={"secondary"}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
         {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
@@ -40,31 +59,7 @@ export default async function Home() {
         </div>
 
         {/* AGENDAMENTO */}
-        <h2 className="mb-3 mt-6 font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3>Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                  <p className="text-sm">Barbearia FSW</p>
-                </Avatar>
-              </div>
-            </div>
-
-            {/* DIREITA */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
@@ -75,7 +70,27 @@ export default async function Home() {
             <BarbershopItem key={barberShop.id} barberShop={barberShop} />
           ))}
         </div>
+
+        {/* RECOMENDADOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarberShops.map((barberShop) => (
+            <BarbershopItem key={barberShop.id} barberShop={barberShop} />
+          ))}
+        </div>
       </div>
+      {/* FOOTER */}
+      <footer>
+        <Card className="px-5 py-6">
+          <CardContent>
+            <p className="text-sm text-gray-400">
+              @ 2023 Copyright <span>FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </main>
   )
 }
