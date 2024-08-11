202b37b3bbddde3d47ea86197103fb9c8472ef88
Lucas <lucaslbmp@github.com>
2024-08-11 11:48:48 -0300

    feat: add header component and banner

---
+++ src/app/_components/ui/header.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent } from "./card"
+import Image from "next/image"
+import { Button } from "./button"
+import { MenuIcon } from "lucide-react"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/logo.svg" alt="FSW Barber" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,33 @@
-import Image from "next/image"
+import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <main className="mb-5"><Button>AAA</Button></main>
+  return (
+    <main className="mb-5">
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Lucas!</h2>
+        <p>Segunda-feira, 05 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative h-[150px] w-full">
+          <Image
+            src="/banner01.png"
+            alt="Agende nos melhores com FSW BArber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </main>
+  )
 }
