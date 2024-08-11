154ebfe6c503698dfc4b7e86235c7dab47f86337
Lucas <lucaslbmp@github.com>
2024-08-10 22:34:16 -0300

    chore: add shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
-  return <main className="mb-5 bg-red-400 text-red-500"></main>
+  return <main className="mb-5"><Button>AAA</Button></main>
 }
