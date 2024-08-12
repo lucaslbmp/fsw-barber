3d21dd6365cf60d0921fbc47ed3569717f0d0b6d
Lucas <lucaslbmp@github.com>
2024-08-12 20:34:25 -0300

    feat: ensure books are listed on bookings page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,31 @@ import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 export default async function Home() {
+  const session = await getServerSession(authOptions)
   const barberShops = await db.barbershop.findMany({})
   const popularBarberShops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
+  const boookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session?.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: { service: { include: { barbershop: true } } },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <main className="mb-5">
@@ -57,7 +74,14 @@ export default async function Home() {
         </div>
 
         {/* AGENDAMENTO */}
-        <BookingItem />
+        <h2 className="mb-3 mt-6 font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {boookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
         {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
