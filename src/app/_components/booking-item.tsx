d15b631026187b4d48bd422137d8ef61cea36473
Lucas <lucaslbmp@github.com>
2024-08-14 17:06:19 -0300

    fix: adding min width to booking item

--- src/app/_components/booking-item.tsx
+++ src/app/_components/booking-item.tsx
@@ -60,141 +60,135 @@ const BookingItem = ({ booking }: BookingItemProps) => {
     setIsSheetOpen(isOpen)
   }
   return (
-    <>
-      <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
-        <SheetTrigger className="w-full">
-          <Card className="min-w-[90%]">
-            <CardContent className="flex justify-between p-0">
-              {/* ESQUERDA */}
-              <div className="flex flex-col gap-2 py-5 pl-5">
-                <Badge
-                  variant={isConfirmed ? "default" : "secondary"}
-                  className="w-fit"
-                >{`${isConfirmed ? "Confirmado" : "Finalizado"}`}</Badge>
-                <h3>{booking.service.name}</h3>
-                <div className="flex items-center gap-2">
-                  <Avatar className="h-6 w-6">
-                    <AvatarImage src={barbershop.imageUrl} />
-                  </Avatar>
-                  <p className="text-sm">{barbershop.name}</p>
-                </div>
+    <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
+      <SheetTrigger className="w-full min-w-[300px]">
+        <Card className="min-w-[90%]">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge
+                variant={isConfirmed ? "default" : "secondary"}
+                className="w-fit"
+              >{`${isConfirmed ? "Confirmado" : "Finalizado"}`}</Badge>
+              <h3>{booking.service.name}</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={barbershop.imageUrl} />
+                </Avatar>
+                <p className="text-sm">{barbershop.name}</p>
               </div>
+            </div>
 
-              {/* DIREITA */}
-              <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-                <p className="text-sm">
-                  {format(booking.date, "MMMM", { locale: ptBR })}
-                </p>
-                <p className="text-2xl">
-                  {format(booking.date, "dd", { locale: ptBR })}
-                </p>
-                <p className="text-sm">
-                  {format(booking.date, "HH:mm", { locale: ptBR })}
-                </p>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">
+                {format(booking.date, "MMMM", { locale: ptBR })}
+              </p>
+              <p className="text-2xl">
+                {format(booking.date, "dd", { locale: ptBR })}
+              </p>
+              <p className="text-sm">
+                {format(booking.date, "HH:mm", { locale: ptBR })}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </SheetTrigger>
+
+      <SheetContent className="w-[90%]">
+        <SheetHeader>
+          <SheetTitle className="text-left">Informações de reserva</SheetTitle>
+        </SheetHeader>
+
+        <div className="relative mt-6 flex h-[180px] w-full items-end">
+          <Image
+            alt={`Mapa da barbearia ${booking.service.barbershop.name}`}
+            src="/map.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+
+          <Card className="z-50 mx-5 mb-3 w-full rounded-xl">
+            <CardContent className="flex items-center gap-3 px-5 py-3">
+              <Avatar>
+                <AvatarImage src={barbershop.imageUrl} />
+              </Avatar>
+              <div>
+                <h3 className="font-bold">{barbershop.name}</h3>
+                <p className="text-xs">{barbershop.address}</p>
               </div>
             </CardContent>
           </Card>
-        </SheetTrigger>
+        </div>
 
-        <SheetContent className="w-[90%]">
-          <SheetHeader>
-            <SheetTitle className="text-left">
-              Informações de reserva
-            </SheetTitle>
-          </SheetHeader>
+        <div className="mt-6">
+          <Badge
+            variant={isConfirmed ? "default" : "secondary"}
+            className="w-fit"
+          >
+            {`${isConfirmed ? "Confirmado" : "Finalizado"}`}
+          </Badge>
 
-          <div className="relative mt-6 flex h-[180px] w-full items-end">
-            <Image
-              alt={`Mapa da barbearia ${booking.service.barbershop.name}`}
-              src="/map.png"
-              fill
-              className="rounded-xl object-cover"
+          <div className="mb-3 mt-6">
+            <BookingSummary
+              barbershop={barbershop.name}
+              service={booking.service}
+              selectedDate={booking.date}
             />
-
-            <Card className="z-50 mx-5 mb-3 w-full rounded-xl">
-              <CardContent className="flex items-center gap-3 px-5 py-3">
-                <Avatar>
-                  <AvatarImage src={barbershop.imageUrl} />
-                </Avatar>
-                <div>
-                  <h3 className="font-bold">{barbershop.name}</h3>
-                  <p className="text-xs">{barbershop.address}</p>
-                </div>
-              </CardContent>
-            </Card>
           </div>
 
-          <div className="mt-6">
-            <Badge
-              variant={isConfirmed ? "default" : "secondary"}
-              className="w-fit"
-            >
-              {`${isConfirmed ? "Confirmado" : "Finalizado"}`}
-            </Badge>
-
-            <div className="mb-3 mt-6">
-              <BookingSummary
-                barbershop={barbershop.name}
-                service={booking.service}
-                selectedDate={booking.date}
-              />
-            </div>
-
-            <div className="space-y-3">
-              {barbershop.phones.map((phone, i) => (
-                <PhoneItem key={i} phone={phone} />
-              ))}
-            </div>
+          <div className="space-y-3">
+            {barbershop.phones.map((phone, i) => (
+              <PhoneItem key={i} phone={phone} />
+            ))}
           </div>
+        </div>
 
-          <SheetFooter className="mt-6">
-            <div className="flex items-center gap-3">
-              <SheetClose asChild>
-                <Button variant="outline" className="w-full">
-                  Voltar
-                </Button>
-              </SheetClose>
-              {isConfirmed && (
-                <Dialog>
-                  <DialogTrigger className="w-full">
-                    <Button variant="destructive" className="w-full">
-                      Cancelar Reserva
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent className="w-[90%]">
-                    <DialogHeader>
-                      <DialogTitle>
-                        Você deseja cancelar sua reserva?
-                      </DialogTitle>
-                      <DialogDescription>
-                        Ao cancelar, você perderá sua reserva e não poderá
-                        recuperá-la. Essa ação é irreversível.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <DialogFooter className="flex flex-row gap-3">
-                      <DialogClose asChild>
-                        <Button variant="secondary" className="w-full">
-                          Voltar
-                        </Button>
-                      </DialogClose>
-                      <DialogClose className="w-full">
-                        <Button
-                          variant="destructive"
-                          onClick={handleCancelBooking}
-                          className="w-full"
-                        >
-                          Confirmar
-                        </Button>
-                      </DialogClose>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog>
-              )}
-            </div>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
-    </>
+        <SheetFooter className="mt-6">
+          <div className="flex items-center gap-3">
+            <SheetClose asChild>
+              <Button variant="outline" className="w-full">
+                Voltar
+              </Button>
+            </SheetClose>
+            {isConfirmed && (
+              <Dialog>
+                <DialogTrigger className="w-full">
+                  <Button variant="destructive" className="w-full">
+                    Cancelar Reserva
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="w-[90%]">
+                  <DialogHeader>
+                    <DialogTitle>Você deseja cancelar sua reserva?</DialogTitle>
+                    <DialogDescription>
+                      Ao cancelar, você perderá sua reserva e não poderá
+                      recuperá-la. Essa ação é irreversível.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter className="flex flex-row gap-3">
+                    <DialogClose asChild>
+                      <Button variant="secondary" className="w-full">
+                        Voltar
+                      </Button>
+                    </DialogClose>
+                    <DialogClose className="w-full">
+                      <Button
+                        variant="destructive"
+                        onClick={handleCancelBooking}
+                        className="w-full"
+                      >
+                        Confirmar
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            )}
+          </div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   )
 }
 
